d376ebf4816198f1adf22bff98d241eb99bb4465
Julia Froegel <froegel.julia@uwlax.edu>
2017-12-14 19:51:45 -0600

    probably changed somthing

--- HW6/logic.pl
+++ HW6/logic.pl
@@ -5,11 +5,19 @@ edge(b,c,10).
 path(X,X,C) :- =(0,C).
 path(X, Y, M) :- edge(Z, Y, P), C is M-P, path(X, Z, C).
 
-
-
+path(X,X,C,[X]):- =(0,C).
+path(X, Y, M, [Y|L]) :- edge(Z, Y, P), C is M-P, path(X, Z, C, L).
 
 grows([_| []]).
 grows([A | [B |T]]) :- <(A, B), grows([B|T]).
 
 sumrunner([],[]).
-sumrunner([A|B], [X|[Y|Z]]) :- X is 0, Y is A+X , sumrunner(B, [Y|Z]). 
+sumrunner([A|B], [X|[Y|Z]]) :- X is 0, Y is A+X , sumrunner(B, [Y|Z]).
+
+sumrunner(A,B) :- sumrunner(A,B,0).
+sumrunner([A|B], [X|[Y|Z]], S) :- X is S, Y is X+A, sumrunner(B, [Y|Z], Y).
+
+
+sumrunner([],[]).
+sumrunner([_|[]],[0|[]]).
+sumrunner([A|[B|C]], [A|[Y|Z]]):- Y is A+B, sumrunner(C,Z).
